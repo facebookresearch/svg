462c58a829f0bb284f5ca44a89b24aea4ede8c78
samuelstanton <ss3765@cornell.edu>
2020-05-19 23:27:33 -0400

    add max_seq_len option

--- scripts/sim_rl_run.py
+++ scripts/sim_rl_run.py
@@ -10,7 +10,7 @@ import torch
 from model_zoo.utils.data import SeqDataset, format_seqs
 
 
-def split_buffer(replay_buffer, max_transitions, subsample_rate):
+def split_buffer(replay_buffer, max_transitions, subsample_rate, min_seq_len=1, max_seq_len=None):
     field_names = ['obses', 'actions', 'rewards', 'next_obses', 'not_dones', 'not_dones_no_max']
     buffer_data = [getattr(replay_buffer, field) for field in field_names]
 
@@ -24,14 +24,13 @@ def split_buffer(replay_buffer, max_transitions, subsample_rate):
         input_seq.append(np.concatenate(transition[:2], axis=1))
         target_seq.append(np.concatenate([transition[2].reshape(1, 1), transition[3] - transition[0]], axis=1))
 
-        if not_done == 0:
-            p = np.random.rand()
-            if p > subsample_rate:
-                input_seq, target_seq = [], []
-                continue
-
-            p = np.random.rand()
-            if p < 0.1:
+        if not_done == 0 or (max_seq_len and len(input_seq) == max_seq_len):
+            p1, p2 = np.random.rand(), np.random.rand()
+            if len(input_seq) < min_seq_len:
+                pass
+            elif p1 > subsample_rate:
+                pass
+            elif p2 < 0.1:
                 test_seqs.append((np.concatenate(input_seq), np.concatenate(target_seq)))
             else:
                 train_seqs.append((np.concatenate(input_seq), np.concatenate(target_seq)))
@@ -78,6 +77,9 @@ def simulate_rl_run(model, dataset, fit_params, train_seqs, test_data, sim_param
 
 
 def experiment(ckpt, cfg):
+    min_seq_len = max(cfg.sim_params.train_seq_len, cfg.sim_params.test_seq_len)
+    assert min_seq_len < cfg.sim_params.max_seq_len
+
     replay_buffer = ckpt.replay_buffer
     env = ckpt.env
     obs_dim = env.observation_space.shape[0]
@@ -85,17 +87,17 @@ def experiment(ckpt, cfg):
 
     cfg.reg_model.params['input_dim'] = obs_dim + action_dim
     cfg.reg_model.params['target_dim'] = 1 + obs_dim
-    # cfg.reg_model.params['model_class'] = cfg.network.name
-    # cfg.reg_model.params['model_kwargs'] = dict(cfg.network.kwargs)
 
     results = []
     for trial in range(cfg.num_seeds):
         print(f"--- TRIAL {trial + 1} ---")
         # format buffer data
         dataset = SeqDataset(cfg.sim_params.train_seq_len, holdout_ratio=cfg.holdout_ratio)
-        train_seqs, test_seqs = split_buffer(replay_buffer, cfg.max_transitions, cfg.subsample_rate)
+        train_seqs, test_seqs = split_buffer(replay_buffer, cfg.max_transitions, cfg.subsample_rate,
+                                             min_seq_len, cfg.sim_params.max_seq_len)
         test_input_seqs = [seq_pair[0] for seq_pair in test_seqs]
         test_target_seqs = [seq_pair[1] for seq_pair in test_seqs]
+        # import pdb; pdb.set_trace()
         test_data = format_seqs(test_input_seqs, test_target_seqs, cfg.sim_params.test_seq_len, 'sequential')
 
         cfg.reg_model.params.obs_dim = obs_dim
